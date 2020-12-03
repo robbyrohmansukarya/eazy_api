2f1ac7a17e7ad758d844ff0a8d77805aca639a3b
robbyrohmansukarya <rbbrohman@gmail.com>
2020-12-03 11:51:49 +0700

    first commmit

---
+++ app/customer_model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class customer_model extends Model
+{
+    protected $table = 'customer';
+    protected $primaryKey = 'customer_id';
+}
---
+++ database/migrations/2020_12_03_041748_customer.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Customer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function(Blueprint $table){
+            $table->bigIncrements('customer_id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('no_telp');
+            $table->string('alamat');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
